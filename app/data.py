7f94ce69fe2878bf07a7bcbdb5575a93d5272d76
Felipe Oliveira <lipefan0@gmail.com>
2024-10-16 17:52:36 -0300

    Criando base do portifolio

---
+++ app/data.py
@@ -0,0 +1,17 @@
+# app/data.py
+
+skills = [
+    {'icon': 'img/icon/python.png', 'skill': 'Python', 'link': 'https://www.python.org/'},
+    {'icon': 'img/icon/flask.png', 'skill': 'Flask', 'link': 'https://flask.palletsprojects.com/'},
+    {'icon': 'img/icon/sql.png', 'skill': 'SQL', 'link': 'https://www.w3schools.com/sql/'},
+    {'icon': 'img/icon/javascript.png', 'skill': 'JavaScript', 'link': 'https://developer.mozilla.org/pt-BR/docs/Web/JavaScript'},
+    {'icon': 'img/icon/tailwindcss.png', 'skill': 'TailwindCSS', 'link': 'https://tailwindcss.com/'},
+    {'icon': 'img/icon/html.png', 'skill': 'HTML', 'link': 'https://developer.mozilla.org/pt-BR/docs/Web/HTML'},
+  ]
+
+projects = [
+    {'name': 'Cadastro de clientes Bling', 'github': 'https://github.com/lipefan0/programa_cadastro_bling_projetos', 'image': 'img/project-1.png'},
+    {'name': 'Projeto 2', 'github': 'https://github.com/seu_usuario/projeto2', 'image': 'img/project-2.png'},
+    {'name': 'Projeto 3', 'github': 'https://github.com/seu_usuario/projeto3', 'image': 'img/project-3.png'},
+    {'name': 'Projeto 4', 'github': 'https://github.com/seu_usuario/projeto4', 'image': 'img/project-4.png'},
+]
---
+++ app/static/javascript/email.js
@@ -0,0 +1,22 @@
+// Inicializa o EmailJS com sua chave pública
+(function() {
+  emailjs.init('ghsQxeOnMzQjby9yi'); // Substitua pelo seu User ID
+})();
+
+window.onload = function() {
+  document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    // Envia o formulário usando o EmailJS
+    emailjs.sendForm('service_cwl8b0b', 'template_gqinv13', this) // Substitua pelos seus IDs
+    .then(() => {
+      console.log('Email enviado com sucesso!');
+      alert('Mensagem enviada com sucesso!');
+      // Limpa o formulário após o envio
+      document.getElementById('contact-form').reset();
+    }, (error) => {
+      console.log('Falha ao enviar: ', error);
+      alert('Falha ao enviar a mensagem.');
+    });
+  });
+}
